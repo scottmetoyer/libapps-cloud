0e423912839b59d787d5d2fd80738ca8694db9d5
Scott Metoyer <scott.metoyer@ucr.edu>
2019-05-06 07:50:16 -0700

    Reactivated the project management features

--- web/js/env.js
+++ web/js/env.js
@@ -3,9 +3,9 @@
   window.__env.auth0ClientId = 'q4H2Kz0W4CgtwGBDjiGIfZHFqXYDvQuT';
   window.__env.auth0Domain = 'ucr-library.auth0.com';
   window.__env.auth0Audience = 'https://ucr-library-libapps-cloud.ucr.edu';
-  window.__env.apiUrl = 'https://j1wzq8z8w5.execute-api.us-west-1.amazonaws.com/dev';
-  window.__env.whitelistUrl = 'j1wzq8z8w5.execute-api.us-west-1.amazonaws.com';
+  window.__env.apiUrl = 'https://5pmknp8mta.execute-api.us-west-1.amazonaws.com/test';
+  window.__env.whitelistUrl = '5pmknp8mta.execute-api.us-west-1.amazonaws.com';
   window.__env.baseUrl = '/';
-  window.__env.applicationUrl = 'http://libapps-cloud.test';
+  window.__env.applicationUrl = 'http://ucr-libapps-cloud-test.s3-website-us-west-2.amazonaws.com';
   window.__env.enableDebug = true;
 }(this));
