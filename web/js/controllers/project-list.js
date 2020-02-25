5a6de6cd18f548583b935ec0ae31cd77bb007e7e
Scott Metoyer <scott.metoyer@ucr.edu>
2020-02-25 08:07:27 -0800

    Presentation mode now follows in flight list sort order

--- web/js/controllers/project-list.js
+++ web/js/controllers/project-list.js
@@ -1,60 +1,71 @@
-(function () {
+;(function() {
   // ===============================================================================
   // Controllers / ProjectList
   //
 
-  function ProjectListCtrl($state, $filter, bl, data, DTDefaultOptions, DTColumnDefBuilder) {
-    var self = this;
-    self.projects = [];
+  function ProjectListCtrl(
+    $state,
+    $filter,
+    bl,
+    data,
+    DTDefaultOptions,
+    DTColumnDefBuilder,
+  ) {
+    var self = this
+    self.projects = []
 
     // Set default page size to 100
-    DTDefaultOptions.setDisplayLength(100);
-    DTDefaultOptions.setOption('stateSave', 'true');
-
+    DTDefaultOptions.setDisplayLength(100)
+    DTDefaultOptions.setOption('stateSave', 'true')
     self.columnDefs = [];
 
-    self.loadProjects = function () {
-      data.getProjects()
-        .then(function (response) {
-          var items = response.data.Items;
+    self.loadProjects = function() {
+      data
+        .getProjects()
+        .then(function(response) {
+          var items = response.data.Items
 
           // Filter the list based on route
           if ($state.current.name == 'projects.backlog') {
-            self.projects = items.filter(function (project) {
-              return (project.executionStatus == 'backlog');
-            });
+            self.projects = items.filter(function(project) {
+              return project.executionStatus == 'backlog'
+            })
           } else if ($state.current.name == 'projects.archive') {
-            self.projects = items.filter(function (project) {
-              return (project.executionStatus == 'archive');
-            });
+            self.projects = items.filter(function(project) {
+              return project.executionStatus == 'archive'
+            })
           } else if ($state.current.name == 'projects.in-flight') {
-            self.projects = items.filter(function (project) {
-              return (project.executionStatus == 'in-flight');
-            });
+            self.projects = items.filter(function(project) {
+              return project.executionStatus == 'in-flight'
+            })
 
-            self.projects.forEach(function (project) {
-              project.statusClass = bl.getStatusClass(project.status);
+            self.projects.forEach(function(project) {
+              project.statusClass = bl.getStatusClass(project.status)
 
-              data.getStatusUpdates(project.id)
-                .then(function (res) {
-                  var list = res.data.Items;
+              data.getStatusUpdates(project.id).then(
+                function(res) {
+                  var list = res.data.Items
 
                   if (list.length > 0) {
-                    list = $filter('orderBy')(list, "timestamp", true);
-                    project.latestUpdate = list[0];
+                    list = $filter('orderBy')(list, 'timestamp', true)
+                    project.latestUpdate = list[0]
                   }
-                }, function (err) {
+                },
+                function(err) {
                   // Error loading status updates for this project
-                  console.log(err);
-                });
+                  console.log(err)
+                },
+              )
             })
           }
-        }).catch(function (err) { console.log(err) });
+        })
+        .catch(function(err) {
+          console.log(err)
+        })
     }
 
-    self.loadProjects();
+    self.loadProjects()
   }
 
-  angular.module('pixeladmin')
-    .controller('ProjectListCtrl', ProjectListCtrl);
-})();
+  angular.module('pixeladmin').controller('ProjectListCtrl', ProjectListCtrl)
+})()
--- web/js/controllers/project-presentation.js
+++ web/js/controllers/project-presentation.js
@@ -1,56 +1,84 @@
-(function () {
-    // ===============================================================================
-    // Controllers / Project Presentation
-    //
+;(function() {
+  // ===============================================================================
+  // Controllers / Project Presentation
+  //
 
-    function ProjectPresentationCtrl($state, $scope, $filter, bl, data) {
-        var self = this;
-        self.projects = [];
+  function ProjectPresentationCtrl($state, $scope, $filter, bl, data) {
+    var self = this
+    self.projects = []
 
-        $scope.initCarousel = function () {
-            $(".owl-carousel").owlCarousel({
-                loop: false,
-                margin: 20,
-                center: true,
-                nav: true,
-                items: 1
-            });
-        };
-
-        self.loadProjects = function () {
-            data.getProjects()
-                .then(function (response) {
-                    var items = response.data.Items;
-
-                    // Filter the list based on route
-                    self.projects = items.filter(function (project) {
-                        return (project.executionStatus == 'in-flight');
-                    });
-
-                    self.projects.forEach(function (project) {
-                        project.statusClass = bl.getStatusClass(project.status);
-
-                        data.getStatusUpdates(project.id)
-                            .then(function (res) {
-                                var list = res.data.Items;
-
-                                if (list.length > 0) {
-                                    list = $filter('orderBy')(list, "timestamp", true);
-                                    project.latestUpdate = list[0];
-                                }
-                            }, function (err) {
-                                // Error loading status updates for this project
-                                console.log(err);
-                            });
-                    })
-                }).catch(function (err) {
-                    console.log(err)
-                });
-        };
-
-        self.loadProjects();
+    $scope.initCarousel = function() {
+      $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 20,
+        center: true,
+        nav: true,
+        items: 1,
+      })
     }
 
-    angular.module('pixeladmin')
-        .controller('ProjectPresentationCtrl', ProjectPresentationCtrl);
-})();
+    self.loadProjects = function() {
+      data
+        .getProjects()
+        .then(function(response) {
+          var items = response.data.Items
+
+          // Filter the list based on route
+          self.projects = items.filter(function(project) {
+            return project.executionStatus == 'in-flight'
+          })
+
+          // Order the items based on the datatables local storage value if it exists
+          var sortColumn = 'lead'
+          var reverse = false;
+
+          try {
+            var dataTablesState = JSON.parse(
+              localStorage.getItem('DataTables_projects_/'),
+            )
+
+            var sortOrder = dataTablesState.order[0]
+            var columns = ['name', 'status', 'lead', 'owner', 'latestUpdate']
+            console.log(self.projects);
+            if (sortOrder[1] == 'desc') {
+              reverse = true
+            }
+            sortColumn = columns[sortOrder[0]]
+
+            console.log(sortColumn + ', reverse: ' + reverse)
+          } catch (e) {
+            console.log('No DataTables state found in local storage.')
+          }
+
+          self.projects = $filter('orderBy')(self.projects, sortColumn, reverse)
+          self.projects.forEach(function(project) {
+            project.statusClass = bl.getStatusClass(project.status)
+
+            data.getStatusUpdates(project.id).then(
+              function(res) {
+                var list = res.data.Items
+
+                if (list.length > 0) {
+                  list = $filter('orderBy')(list, 'timestamp', true)
+                  project.latestUpdate = list[0]
+                }
+              },
+              function(err) {
+                // Error loading status updates for this project
+                console.log(err)
+              },
+            )
+          })
+        })
+        .catch(function(err) {
+          console.log(err)
+        })
+    }
+
+    self.loadProjects()
+  }
+
+  angular
+    .module('pixeladmin')
+    .controller('ProjectPresentationCtrl', ProjectPresentationCtrl)
+})()
